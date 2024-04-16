4e312f40728e095365028329753afaf0335bdda8
Ayman Isam <aymanisam26@gmail.com>
2024-04-16 13:55:14 +0200

    More subjects

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
+const examName = document.getElementById('examName');
 
-const newYearTime = new Date('April 24 2024 00:00:00');
+const examDates = {
+    'IB Exams': 'April 24 2024 00:00:00',
+    'Chemistry Paper 1': 'May 8 2024 13:30:00',
+    'Chemistry Paper 2': 'May 8 2024 14:30:00',
+    'Chemistry Paper 3': 'May 9 2024 09:30:00'
+};
+
+let newYearTime = new Date(examDates[localStorage.getItem('selectedExam') || 'IB Exams']);
+examName.textContent = localStorage.getItem('selectedExamText') || 'IB Exams';
 
 function updateCountDownTime() {
     const currentTime = new Date();
@@ -20,6 +29,32 @@ function updateCountDownTime() {
     seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
-setInterval(updateCountDownTime, 1000);
+let timer = setInterval(() => updateCountDownTime(newYearTime));
+
+const navbar = document.getElementById('navbar');
+const navButton = document.getElementById('navButton');
+const crossButton = document.getElementById('crossButton');
+
+navButton.addEventListener('click', function() {
+    navbar.classList.toggle('hidden');
+});
+
+crossButton.addEventListener('click', function() {
+    navbar.classList.toggle('hidden');
+});
+
+const links = navbar.getElementsByTagName('a');
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function() {
+        clearInterval(timer);
+        newYearTime = new Date(examDates[this.textContent]);
+        console.log('Link text content:', this.textContent);
+        localStorage.setItem('selectedExam', this.textContent);
+        localStorage.setItem('selectedExamText', this.textContent);
+        examName.textContent = this.textContent;
+        timer = setInterval(() => updateCountDownTime(newYearTime), 1000);
+    });
+}
+
 
 
